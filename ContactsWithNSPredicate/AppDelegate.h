ab241a4ef33805eaa45269b67045dfaad2e340ea
user <user@glbitms-imac-58.local>
2017-08-09 03:14:21 -0700

    Initial Commit

---
+++ ContactsWithNSPredicate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContactsWithNSPredicate
+//
+//  Created by user on 09/08/17.
+//  Copyright © 2017 glbitm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ContactsWithNSPredicate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContactsWithNSPredicate
+//
+//  Created by user on 09/08/17.
+//  Copyright © 2017 glbitm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
