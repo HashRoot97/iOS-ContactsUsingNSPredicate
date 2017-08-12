3e9769ac0aac49828b8cb5bdce7e3332d22bb099
Admin <admin@Mac-Admin.local>
2017-08-12 14:11:57 +0300

    Initial Commit

--- ContactsWithNSPredicate/ViewController.m
+++ ContactsWithNSPredicate/ViewController.m
@@ -16,7 +16,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    myArray = [[NSMutableArray alloc] init];
+    NSArray *array = @[@"Akshat", @"Eric", @"Angad", @"Zain", @"Bosch", @"Kyle", @"Kevin", @"Stan", @"Emily"
+                       , @"Butters", @"Wendy", @"Sunny"];
+    
+    for(char c='A'; c<='Z'; c++){
+        NSString *string = [NSString stringWithFormat:@"%c", c];
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF beginswith[c] %@", string];
+        NSArray *result = [array filteredArrayUsingPredicate:predicate];
+        if(result.count >0){
+            [myArray addObject:result];
+        }
+    }
+    NSLog(@"%@", myArray);
 }
 
 
@@ -25,5 +37,45 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Table View Data Source
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    NSArray *tempArray = [myArray objectAtIndex:section];
+    return [tempArray count];
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return [myArray count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *cellId = @"SimpleTableId";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    if(cell == nil){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+    }
+    NSArray *tempArray = [myArray objectAtIndex:indexPath.section];
+    NSString *str = [tempArray objectAtIndex:indexPath.row];
+    cell.textLabel.text = str;
+    return cell;
+}
+
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    
+    NSArray *tempArray = [myArray objectAtIndex:section];
+    
+    NSString *key =  [[[tempArray objectAtIndex:0] substringToIndex: 1] uppercaseString];
+    return key;
+    
+}
+
+#pragma mark - TableView Delegate
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    NSArray *tempArray = [myArray objectAtIndex:indexPath.section];
+    NSString *str = [tempArray objectAtIndex:indexPath.row];
+    NSLog(@"%@", str);
+}
 @end
